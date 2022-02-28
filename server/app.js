117a16e3be495aa9325bb2d1663187706a00d965
mayraochoa13 <mayochoa@csumb.edu>
2022-02-27 22:04:20 -0800

    server created

--- server/app.js
+++ server/app.js
@@ -1,2 +1,11 @@
+//jshint esversion:6
+const express = require("express");
 
+const app = express();
+
+const mongoose  = require("mongoose");
+
+app.listen(3000, function(){
+    console.log("Server started on port 3000");
+});
 
