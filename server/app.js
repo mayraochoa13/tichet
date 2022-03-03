67135b562ddc6c4be2389df672c070b347ded366
saulRM10 <43281985+saulRM10@users.noreply.github.com>
2022-03-02 18:14:19 -0800

    created sample db model, imported it to app.js, connected databse to mongo db atlas

--- server/app.js
+++ server/app.js
@@ -1,11 +1,77 @@
 //jshint esversion:6
+
+
+const { response } = require("express");
 const express = require("express");
 
 const app = express();
 
 const mongoose  = require("mongoose");
 
+// import database schema 
+//import Ticket from "./models/ticket"; 
+
+// import model 
+const Sample = require('./models/sample'); 
+// connect mongoose database 
+// mongoose.connect("mongodb://localhost:27017/TicketsDB" , {useNewUrlParser: true , useUnifiedTopology: true}); 
+
+// // schema 
+// const TicketSchema = new mongoose.Schema({
+
+//     title:{
+//         type: String , 
+//         required: true, 
+//     },
+//     description:{
+//         type: String, 
+//         required: true,
+//     },
+// }); 
+
+// create model 
+//const Ticket = mongoose.model("Ticket", TicketSchema); 
+
+//let ticket01 = { title: 'broken screen' , description: 'bullet hole'};
+
+//Ticket.insertMany(ticket01); 
+
+mongoose.connect("mongodb+srv://adminSaul:test123@cluster0.pyekv.mongodb.net/SampleDB?retryWrites=true&w=majority" , {useNewUrlParser: true , useUnifiedTopology: true}); 
+
+//let samp01 = { name: 'saul' , age: '24'}; 
+//Sample.insertMany(samp01);
+
+// let samp02 = { name: 'luis' , age: '23'}; 
+//  Sample.insertMany(samp02);
+
+// let samp04 = { name: 'mayra' , age: '27'}; 
+//  Sample.insertMany(samp04);
+
+app.get("/", function(require, response){
+    response.send("hello world ");
+})
+
+//app.post () 
+
 app.listen(3000, function(){
     console.log("Server started on port 3000");
+    
 });
 
+// npm install mongoose
+
+
+// next steps 
+// 1) import database schema + model (x)
+// 2) connect it to Mongodb Atlas 
+// 3) create the from/ticket 
+//      - route "/newTicket"
+//     - get the user input using ejs, body-parser 
+//     -  finish the route , we have the data now we just need to insert to db 
+// 4) creat dashboard with ticket summary data <input name= "status"
+// == from users can submit , a dashboard one can see a list of submissions 
+//        4 a) query and find all the tickets made 
+//          b) display 
+//          c) query for specific tickets and find them and display them 
+// 
+// CRUD = > create , read , update , delete 
---
+++ server/models/sample.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const sampleSchema = new mongoose.Schema({
+
+    name :{
+        type: String , 
+        required: true, 
+    },
+    age:{
+        type: String, 
+        required: true,
+    }
+
+}); 
+
+const Sample = mongoose.model("samples", sampleSchema); 
+
+module.exports = Sample; 
+
