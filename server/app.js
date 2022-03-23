163b50e036bdff295687ebdb9712d225d8a351a9
mayraochoa13 <mayochoa@csumb.edu>
2022-03-23 15:42:00 -0700

    test

--- server/app.js
+++ server/app.js
@@ -8,51 +8,22 @@ const app = express();
 
 const mongoose  = require("mongoose");
 
-// going to use three packages and installing using npm 
-
-// express session 
-const session = require('express-session'); 
-
-// passport js 
-const passport = require('passport'); 
-
-// passport-local-mongoose 
-const passportLocalMongoose = require('passport-local-mongoose'); 
-
-// also passport-local but i do not need to require it but it was installed 
-// npm i passport passport-local passport-local-mongoose express-session 
-
-
 // require body parser 
 const bodyParser = require("body-parser"); 
 
 // use ejs 
 app.set('view engine' , 'ejs');
 
+
 // use body parser 
 app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({extended:true }));
 
-// connecting css to the server
-const path = require('path')
-app.use('/static', express.static(path.join(__dirname, 'public')))
-
-// start session 
-app.use(session({
-    secret:"any string",
-    resave: false,
-    saveUninitialized: false
-})); 
-
-// initialize passport 
-app.use(passport.initialize());
-
-// use passport to deal with session 
-app.use(passport.session()); 
-
 
 // import database schema 
 //import Ticket from "./models/ticket"; 
+
+// import model 
 const Sample = require('./models/sample'); 
 // connect mongoose database 
 // mongoose.connect("mongodb://localhost:27017/TicketsDB" , {useNewUrlParser: true , useUnifiedTopology: true}); 
@@ -60,49 +31,30 @@ const Sample = require('./models/sample');
 
 mongoose.connect("mongodb+srv://adminSaul:test123@cluster0.pyekv.mongodb.net/SampleDB?retryWrites=true&w=majority" , {useNewUrlParser: true , useUnifiedTopology: true}); 
 
-// i attempted to create a new model/schema outside in the ./model but i need to work with passport 
-// so i will figure out how to move this user model/schema to ./model later 
+//let samp01 = { name: 'saul' , age: '24'}; 
+//Sample.insertMany(samp01);
 
-const userSchema= new mongoose.Schema( { 
-    email:  String, 
-    password: String 
-});
-
-// add passport local mongoose as a plug in for userSchema 
-userSchema.plugin(passportLocalMongoose); 
-
-const User = new mongoose.model("User", userSchema); 
-
-// passport local configurations 
-passport.use(User.createStrategy()); 
-
-// serialise creates a cookie 
-passport.serializeUser(User.serializeUser()); 
-
-// decode cookie message 
-passport.deserializeUser(User.deserializeUser()); 
 
 // activate filter value 
+// let filterName = 0 ; 
+// let noFilter  = 0 ; 
+// let filterAge = 0 ; 
 let filterVal = 0;
 
 
-app.get("/", function(req, res){
+// timestamp in seconds 
 
-// this is the route we want to make sure user is authenticated 
 
-if( req.isAuthenticated()){// check authentication 
+
+app.get("/", function(require, response){
+
+
 // console.log( "no filter : " + noFilter);
 // console.log( "filter by name : " + filterName );
 // console.log("filter by age : " + filterAge);
 
-// console.log("----------------------------"); 
 
-    // if(noFilter === 0 || filterName === undefined && filterAge == undefined ){}
-
-    // if filter button is pressed then filter items 
-    //noFilter === undefined && filterAge == undefined 
-
-
+    
     // 1) filters by name
     // 2) filters by age
     // 3) undo the work
@@ -110,7 +62,7 @@ if( req.isAuthenticated()){// check authentication
         case "1":
             Sample.find().sort({name:1}).exec(function( err, sortedUsers){
                 if( !err ){
-                    res.render('dashboard' , { dataList : sortedUsers }); 
+                    response.render('dashboard' , { dataList : sortedUsers }); 
                 }
             });
             break;
@@ -118,7 +70,7 @@ if( req.isAuthenticated()){// check authentication
             Sample.find().sort({age:1}).exec(function( err, sortedUsersByAge){
 
                 if( !err ){
-                    res.render('dashboard' , { dataList : sortedUsersByAge }); 
+                    response.render('dashboard' , { dataList : sortedUsersByAge }); 
                 }
             });
             break;   
@@ -126,17 +78,11 @@ if( req.isAuthenticated()){// check authentication
             Sample.find({}, function( err, foundUsers){
             // found all the documents and stored them on found users 
                 if( ! err){
-                    res.render('dashboard', { dataList : foundUsers}); 
+                    response.render('dashboard', { dataList : foundUsers}); 
                 }
             }); 
             break;
     }
-}// end authentication if statement 
-
-else{
-    // they are not authenticated, send them to log in 
-    res.redirect('/login'); 
-}
 
 }); 
 
@@ -151,66 +97,6 @@ app.post("/trigger", function( require, response){
     response.redirect("/")
 
 }); 
-
-app.get("/home", function( req, res){
-    res.render("home"); 
-}); 
-
-
-app.get("/login", function( req , res ){
-    res.render("login");
-}); 
-
-app.get('/register', function( req , res ){
-    res.render("register");
-}); 
-
-// lets collect user details to register and authenticate 
-app.post('/register', function( req , res ){
-    
-    // use method register from the passport mongoose package 
-User.register({username: req.body.username}, req.body.password, function( err, user){
-
-    if( err ){
-        console.log( err ); 
-        res.redirect("/register"); 
-    }
-    //else {
-        // if no errors we will authenticate our user using passport 
-        // we are using 'local' strategy 
-            passport.authenticate("local")(req, res, function(){
-                // we authenticated them, so let them see the '/' which has the dashboard 
-                res.redirect("/"); 
-            })
-        }); 
-
-    
-}); 
-
-// let users log in 
-app.post('/login' , function( req, res){
-
-    // create user 
-    const user = new User({
-        username: req.body.username,
-        password: req.body.password
-    }); 
-
-    // log in the user with passport 
-
-    req.login( user , function(err){
-        if(err){
-            console.log(err);
-        }
-
-        // create and send a cookie to browser to let it know user are logged in 
-        passport.authenticate("local")(req, res, function(){
-            // they are allowed to see dashboard 
-            res.redirect('/'); 
-        })
-    })
-}); 
-
 
 
 app.get("/newUser" , function( require, response){
@@ -259,13 +145,6 @@ app.post("/delete", function(require, response){
         })
 }); 
 
-
-app.get('/logout', function( req, res){
-    req.logout();
-
-    // log out and take to log in / register page 
-    res.redirect('/home');
-});
 app.listen(3000, function(){
     console.log("Server started on port 3000");
     
@@ -281,7 +160,8 @@ app.listen(3000, function(){
 // 3) create the from/ticket 
 //      - route "/newTicket"
 //     - get the user input using ejs, body-parser 
-//     -  finish the route , we have the data now we just need to insert to db 
+//     -  finish the route , we have the data now we just need to insert to db
+// 3.5 User Authentication  
 // 4) creat dashboard with ticket summary data <input name= "status"
 // == from users can submit , a dashboard one can see a list of submissions 
 //        4 a) query and find all the tickets made 
@@ -292,3 +172,4 @@ app.listen(3000, function(){
 
 
 // if " cannot find x_dependency  " { npm i x_dependency }
+//klfjkfhkhdjkldfhs'sdfbnk;
