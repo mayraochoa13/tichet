837265cab4a71c6b40e5ed4752f61f01e54cee72
mayraochoa13 <mayochoa@csumb.edu>
2022-04-11 18:59:45 -0700

    fixed typo

--- server/app.js
+++ server/app.js
@@ -693,7 +693,7 @@ app.post('/updateDelete', function(req, res){
      }); 
 }); 
 let port = process.env.PORT;
-if(port==null||post == ""){
+if(port==null||port == ""){
     port = 3000;
 }
 app.listen(port, function(){
