d490b5b4a004366c1cd03e60a349fa146039c257
mayraochoa13 <mayochoa@csumb.edu>
2022-04-11 18:01:02 -0700

    changed route

--- server/app.js
+++ server/app.js
@@ -693,7 +693,7 @@ app.post('/updateDelete', function(req, res){
      }); 
 }); 
 
-app.listen(3000, function(){
+app.listen(process.env.PORT||3000, function(){
     console.log("Server started on port 3000");
    
 });
