02c5f4b85054dc7639f84b29caa6e197691f7639
mayraochoa13 <mayochoa@csumb.edu>
2022-04-11 18:50:33 -0700

    Add dependecies, .git ignore

--- server/app.js
+++ server/app.js
@@ -692,9 +692,12 @@ app.post('/updateDelete', function(req, res){
         }   
      }); 
 }); 
-
-app.listen(process.env.PORT||3000, function(){
-    console.log("Server started on port 3000");
+let port = process.env.PORT;
+if(port==null||post == ""){
+    port = 3000;
+}
+app.listen(port, function(){
+    console.log("Server has started successfully");
    
 });
 
